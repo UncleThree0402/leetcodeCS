2719f6ea80f97873d2e1e2aa5a954a79c0c257a0
UncleThree0402 <wonghokchi0402@gmail.com>
2021-10-17 19:40:21 +0800

    RumTime 120ms, Memory Usage 43MB

---
+++ Two Sum/TwoSum.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace Two_Sum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        private int[] solve(int[] nums, int target)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index;
+                if (dictionary.TryGetValue(target - nums[i], out index))
+                {
+                    return new int[]{i, index};
+                }
+                if(!dictionary.ContainsKey(nums[i]))
+                {
+                    dictionary.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
